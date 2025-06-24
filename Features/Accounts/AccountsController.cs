da61bd8c1a2c2be23d4f760cfa2f11d60090db68
Dean Agan <agandfr@gmail.com>
2025-06-24 21:28:53 +1000

    Add available

--- Features/Accounts/AccountsController.cs
+++ Features/Accounts/AccountsController.cs
@@ -11,6 +11,7 @@ namespace account_service.Features.Accounts
             public int Id { get; set; }
             public required string Name { get; set; }
             public decimal Balance { get; set; }
+            public decimal Available { get; set; }
         }
 
         // GET: api/accounts
@@ -19,9 +20,9 @@ namespace account_service.Features.Accounts
         {
             var accounts = new[]
             {
-                new Account { Id = 1, Name = "Savings", Balance = 1000.50m },
-                new Account { Id = 2, Name = "Checking", Balance = 500.25m },
-                new Account { Id = 3, Name = "Investment", Balance = 12000.00m }
+                new Account { Id = 1, Name = "Savings", Balance = 1000.50m, Available = 1000.50m },
+                new Account { Id = 2, Name = "Checking", Balance = 500.25m, Available = 500.25m },
+                new Account { Id = 3, Name = "Investment", Balance = 12000.00m, Available = 12000.00m }
             };
 
             return Ok(accounts);
